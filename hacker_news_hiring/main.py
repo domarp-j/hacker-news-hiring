0b08e144b00e4ba00b42c6cee746830ffea24b0d
Pramod Jacob <psjacob68@gmail.com>
2020-06-01 21:36:29 -0400

    Refine job search criteria

--- hacker_news_hiring/main.py
+++ hacker_news_hiring/main.py
@@ -3,28 +3,73 @@ import csv
 import re
 
 
+# "Who is hiring?" Hacker News post ID (June 2020)
+POSTING_ID = 23379196
+
+# Search text with following regex patterns (case-insensitive)
+LOCATION_PATTERN = "(remote)|(san diego)"
+CAREER_PATTERN = "(fullstack)|(full-stack)|(frontend)|(front-end)|(backend)|(back-end)|(software engineer)"
+
+# CSV name
+CSV_NAME = "hacker_news_postings.csv"
+
+
+# ==================================================
+# Helpers
+# ==================================================
+
+
 def call_hacker_news_api(id=None):
   return requests.get(
     'https://hacker-news.firebaseio.com/v0/item/{id}.json'.format(id=id)
   )
 
 
-# ==================================================
+# Create an in-memory cache containing HN posts that have already been queried
+def store_existing_csv_data():
+  try:
+    comment_ids = {}
+    with open(CSV_NAME) as csv_file:
+      for row in csv.reader(csv_file, delimiter=','):
+        comment_ids[row[0]] = True
+    return comment_ids
+  except FileNotFoundError:
+    print("CSV not found.")
+    return {}
 
+
+# Generate CSV with postings matching criteria above
+def write_csv(who_is_hiring={}):
+  with open(CSV_NAME, 'a+') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+
+    for comment_id in who_is_hiring["kids"]:
+      print("Fetching post with ID {id}...".format(id=comment_id))
+      posting = call_hacker_news_api(id=comment_id)
+
+      if "text" not in posting.json():
+        continue
+
+      location_match = re.search(LOCATION_PATTERN, posting.json()["text"].lower())
+      career_match = re.search(CAREER_PATTERN, posting.json()["text"].lower())
+
+      if location_match and career_match:
+        print("Match! Storing in CSV...")
+        writer.writerow([
+          comment_id,
+          posting.json()["text"].replace('\n', "\\n").strip()
+        ])
+
+
+# ==================================================
+# Main
+# ==================================================
 
 def run():
   print("Fetching API data for \"Who is hiring?\" post...")
-  who_is_hiring = call_hacker_news_api(id=23379196)
+  who_is_hiring = call_hacker_news_api(id=POSTING_ID)
 
   print("Fetching postings...")
-  with open('hacker_news_postings.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
+  write_csv(who_is_hiring=who_is_hiring.json())
 
-    for comment_id in who_is_hiring.json()["kids"]:
-      posting = call_hacker_news_api(id=comment_id)
-
-      if re.search("(remote)|(san diego)", posting.json()["text"].lower()):
-        writer.writerow([
-          comment_id,
-          posting.json()["text"].replace('\n', ' ').strip()
-        ])
+  print("Done!")
