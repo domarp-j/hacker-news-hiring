f253f827ff4a497fadbd1664f448598f91512d4c
Pramod Jacob <psjacob68@gmail.com>
2020-06-01 22:58:27 -0400

    Add post blacklisting

--- hacker_news_hiring/main.py
+++ hacker_news_hiring/main.py
@@ -11,12 +11,24 @@ LOCATION_PATTERN = "(remote)|(san diego)"
 CAREER_PATTERN = "(fullstack)|(full-stack)|(frontend)|(front-end)"
 
 # CSV name
-CSV_NAME = "postings.csv"
-
+POSTING_CSV = "postings.csv"
+BLACKLIST_CSV = "blacklist.csv"
 
 # ==================================================
 # Helpers
 # ==================================================
+
+
+def generate_blacklist_cache():
+  try:
+    blacklist = {}
+    with open(BLACKLIST_CSV, 'r') as csv_file:
+      for row in csv.reader(csv_file, delimiter=','):
+        blacklist[row[0]] = True
+    return blacklist
+  except FileNotFoundError:
+    print("CSV not found. Continuing...")
+    return {}
 
 
 def call_hacker_news_api(id=None):
@@ -26,11 +38,15 @@ def call_hacker_news_api(id=None):
 
 
 # Generate CSV with postings matching criteria above
-def write_csv(who_is_hiring={}):
-  with open(CSV_NAME, 'a+') as csv_file:
+def write_csv(who_is_hiring={}, blacklist={}):
+  with open(POSTING_CSV, 'a+') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
 
     for comment_id in who_is_hiring["kids"]:
+      if str(comment_id) in blacklist:
+        print("Ignoring blacklisted post with ID {id}...".format(id=comment_id))
+        continue
+
       print("Fetching post with ID {id}...".format(id=comment_id))
       posting = call_hacker_news_api(id=comment_id)
 
@@ -53,10 +69,13 @@ def write_csv(who_is_hiring={}):
 # ==================================================
 
 def run():
+  print("Storing blacklist in memory...")
+  blacklist = generate_blacklist_cache()
+
   print("Fetching API data for \"Who is hiring?\" post...")
   who_is_hiring = call_hacker_news_api(id=POSTING_ID)
 
   print("Fetching postings...")
-  write_csv(who_is_hiring=who_is_hiring.json())
+  write_csv(who_is_hiring=who_is_hiring.json(), blacklist=blacklist)
 
   print("Done!")
